a3ff8553c51ee4dbd5ffd6975987d095226271e0
YurecUA <81627173+YurecUA@users.noreply.github.com>
2022-11-06 19:05:24 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+// Напиши скрипт управления формой логина.
 
+// Обработка отправки формы form.login-form должна быть по событию submit.
+// При отправке формы страница не должна перезагружаться.
+// Если в форме есть незаполненные поля, выводи alert с предупреждением о том,
+// что все поля
+// должны быть заполнены.
+// Если пользователь заполнил все поля и отправил форму, собери значения полей
+// в обьект,
+// где имя поля будет именем свойства, а значение поля - значением свойства.
+// Для доступа к элементам формы используй свойство elements.
+// Выведи обьект с введенными данными в консоль и очисти значения полей
+// формы методом reset.
+
+const formEl = document.querySelector('.login-form');
+const inputEmail = document.querySelector('input[type="email"]');
+const inputPassword = document.querySelector('input[type="password"]');
+const buttonEl = document.querySelector('button[type="submit"]');
+
+formEl.addEventListener('submit', e => {
+  inputEmail.value && inputPassword.value
+    ? console.log({
+        email: inputEmail.value,
+        password: inputPassword.value,
+      })
+    : alert('Fill all fields!');
+  e.preventDefault();
+});
